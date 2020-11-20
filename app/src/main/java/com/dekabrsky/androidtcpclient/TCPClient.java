47d6d618061f470938201c92430e2eefdb4fd337
Denis <dmatafonov2000@mail.ru>
2020-11-20 14:50:05 +0500

    new adr

--- app/src/main/java/com/dekabrsky/androidtcpclient/TCPClient.java
+++ app/src/main/java/com/dekabrsky/androidtcpclient/TCPClient.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 public class TCPClient {
 
     private String serverMessage;
-    public static final String SERVERIP = "192.168.0.108"; //поставь IP своего компьютера
+    public static final String SERVERIP = "192.168.0.103"; //поставь IP своего компьютера
     public static final int SERVERPORT = 4444;
     private OnMessageReceived mMessageListener = null;
     private boolean mRun = false;
